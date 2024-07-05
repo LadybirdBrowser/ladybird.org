95264feb10135cbf0d38c5562a2b93f594d5c21c
versecafe <147033096+versecafe@users.noreply.github.com>
2024-07-05 03:22:25 -0400

    full port to astro + tailwind support

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), sitemap()],
+});

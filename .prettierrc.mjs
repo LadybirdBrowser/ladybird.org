f78c17195bfbfd7e1d6927e0327b9d5e19674f46
versecafe <147033096+versecafe@users.noreply.github.com>
2024-07-05 03:22:25 -0400

    full port to astro + tailwind support

---
+++ .prettierrc.mjs
@@ -0,0 +1,21 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
+
+export default config;
---
+++ README.md
@@ -0,0 +1,15 @@
+# ladybird.org
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun dev
+```
+
+> Styling is currently coming from standard css in `src/styles/global.css` but will be tailwind driven in the future, you can already use tailwind inside any `class=""` block right now if you are making a PR

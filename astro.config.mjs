0935426dd4768922889f94f6e61c370de8c861b7
versecafe <147033096+versecafe@users.noreply.github.com>
2024-07-05 17:28:52 -0400

    github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ladybird.org",
   integrations: [tailwind(), sitemap()],
 });

f3f4c68cbc8ea5770af88ec60d9a315a04ef4192
Samuel Fry <samuel.fry13@gmail.com>
2024-07-16 22:16:06 -0400

    rss: add simple rss feed
    rss: add tests to check against generated XML

---
+++ tests/rss.test.ts
@@ -0,0 +1,45 @@
+import { test, expect, describe } from "bun:test";
+import { XMLParser } from "fast-xml-parser";
+import fs from "fs";
+import path from "path";
+
+const rootDir: string = path.join(__dirname, "../");
+
+describe("RSS Feeds", () => {
+  const srcDir = path.join(rootDir, "/src/content/posts");
+  const distDir = path.join(rootDir, "/dist/");
+
+  const mdFiles = fs
+    .readdirSync(srcDir)
+    .filter((file) => file.endsWith(".mdx"));
+  const xmlFile = fs.readFileSync(path.join(distDir, "posts.xml"));
+
+  const parser = new XMLParser();
+  const parsedXML = parser.parse(xmlFile);
+
+  test("RSS Channel includes overall metadata", () => {
+    expect(parsedXML.rss.channel).toEqual(
+      expect.objectContaining({
+        title: "Ladybird Browser Posts",
+        description: "Ladybird is a brand-new browser &amp; web engine",
+        link: "https://ladybird.org",
+      })
+    );
+  });
+
+  test("XML should contain entries for every post", async () => {
+    expect(parsedXML.rss.channel.item).toBeArrayOfSize(mdFiles.length);
+    parsedXML.rss.channel.item.forEach((item: any) => {
+      const itemAttributes = Object.keys(item);
+      expect(itemAttributes).toEqual(
+        expect.arrayContaining([
+          "title",
+          "link",
+          "description",
+          "pubDate",
+          "author",
+        ])
+      );
+    });
+  });
+});

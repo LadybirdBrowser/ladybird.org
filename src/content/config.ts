a162d7deca7f0181e2e688e2956cc63fcce37f5a
Samuel Fry <samuel.fry13@gmail.com>
2024-07-27 18:11:37 -0400

    Add prop types and clean up layout props

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,40 @@
 import { z, defineCollection } from "astro:content";
 
-const posts = defineCollection({
-  type: "content",
-  schema: z.object({
+export const postSchema = z
+  .object({
     title: z.string().max(50),
     description: z.string().max(280),
     author: z.string().max(50),
     image: z.string().min(12),
     type: z.enum(["Thoughts", "Announcement", "Hidden"]),
     date: z.date(),
-  }),
+  })
+  .strict();
+export type Post = z.infer<typeof postSchema>;
+
+const posts = defineCollection({
+  type: "content",
+  schema: postSchema,
 });
 
-const newsletter = defineCollection({
-  type: "content",
-  schema: z.object({
+export const newsletterSchema = z
+  .object({
+    title: z
+      .string()
+      .regex(
+        /This Month in Ladybird - \w+ \d{4}/,
+        'Newsletter title must be of shape: "This Month in Ladybird - <month> <year>"'
+      ),
     description: z.string().max(280),
     date: z.date(),
     draft: z.boolean().optional(),
-  }),
+  })
+  .strict();
+export type Newsletter = z.infer<typeof newsletterSchema>;
+
+const newsletter = defineCollection({
+  type: "content",
+  schema: newsletterSchema,
 });
 
 export const collections = {
--- tests/newsletters.test.ts
+++ tests/newsletters.test.ts
@@ -12,17 +12,41 @@ describe("Newsletters", () => {
     .readdirSync(srcDir)
     .filter((file) => file.endsWith(".mdx"));
 
-  test("Draft pages should be excluded in build", async () => {
+  test("Draft pages should be excluded in build", () => {
     mdFiles.forEach((file) => {
       const filePath = path.join(srcDir, file);
       const htmlFile = file.replace(".mdx", "/index.html");
       const htmlFilePath = path.join(distDir, htmlFile);
 
-      const fileContent = fs.readFileSync(filePath);
-      if (fileContent.toString().includes("draft: true")) {
+      const fileContent = fs.readFileSync(filePath, "utf-8");
+      if (fileContent.includes("draft: true")) {
         expect(fs.existsSync(htmlFilePath)).toBe(false);
       } else {
         expect(fs.existsSync(htmlFilePath)).toBe(true);
+      }
+    });
+  });
+
+  test("Newsletters should include title and description metadata", () => {
+    mdFiles.forEach((file) => {
+      const filePath = path.join(srcDir, file);
+      const htmlFile = file.replace(".mdx", "/index.html");
+      const htmlFilePath = path.join(distDir, htmlFile);
+
+      const mdFileContent = fs.readFileSync(filePath, "utf-8");
+      if (!mdFileContent.includes("draft: true")) {
+        const htmlFileContent = fs.readFileSync(htmlFilePath, "utf-8");
+        const expectedTitle =
+          mdFileContent.match(/^title:\s*(.*)$/m)?.[1] + " - Ladybird";
+        const expectedDescription = mdFileContent.match(
+          /^description:\s*(.*)$/m
+        )?.[1];
+
+        const metaTitle = `<meta property="og:title" content="${expectedTitle}">`;
+        const metaDescription = `<meta property="og:description" content="${expectedDescription}">`;
+
+        expect(htmlFileContent).toContain(metaTitle);
+        expect(htmlFileContent).toContain(metaDescription);
       }
     });
   });

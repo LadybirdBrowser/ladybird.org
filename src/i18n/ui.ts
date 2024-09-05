1aa83fac292d3b4ceada3b61daafc113e10c07b4
Engin Karakurt <155079545+engin-karakurt@users.noreply.github.com>
2024-09-05 22:54:15 +0200

    i18n: Translate navigation and language-picker (en, de)

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,10 +7,25 @@ export const defaultLang = "en";
 
 export const ui = {
   en: {
-    "nav.home": "Home",
+    // Navigation (en)
     "nav.about": "About",
+    "nav.news": "News",
+    "nav.gi": "Get Involved",
+    "nav.sponsors": "Sponsors",
+    "nav.faq": "FAQ",
+    "nav.donate": "Donate",
+    // Language Picker (en)
+    "language-picker.language": "Language",
   },
   de: {
+    // Navigation (de)
     "nav.about": "Über uns",
+    "nav.news": "Neuigkeiten",
+    "nav.gi": "Mitmachen",
+    "nav.sponsors": "Sponsoren",
+    "nav.faq": "FAQ",
+    "nav.donate": "Spenden",
+    // Language Picker (de)
+    "language-picker.language": "Sprache",
   },
 } as const;

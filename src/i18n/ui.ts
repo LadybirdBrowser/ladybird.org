ce42cfadf8c5793cac646a22d4451adc43775915
Engin Karakurt <155079545+engin-karakurt@users.noreply.github.com>
2024-09-05 23:05:00 +0200

    i18n: Translate hero section & adjust comments

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -7,25 +7,39 @@ export const defaultLang = "en";
 
 export const ui = {
   en: {
-    // Navigation (en)
+    // Navigation
     "nav.about": "About",
     "nav.news": "News",
     "nav.gi": "Get Involved",
     "nav.sponsors": "Sponsors",
     "nav.faq": "FAQ",
     "nav.donate": "Donate",
-    // Language Picker (en)
+    // Language Picker
     "language-picker.language": "Language",
+    // Hero
+    "hero.welcomeTo": "Welcome to ",
+    "hero.aTrulyIndependent": "a truly independent",
+    "hero.webBrowser": "web browser.",
+    "hero.paragraph":
+      "We are building a brand-new browser from scratch, backed by a non-profit.",
+    "hero.getInvolved": "Get Involved",
   },
   de: {
-    // Navigation (de)
+    // Navigation
     "nav.about": "Über uns",
     "nav.news": "Neuigkeiten",
     "nav.gi": "Mitmachen",
     "nav.sponsors": "Sponsoren",
     "nav.faq": "FAQ",
     "nav.donate": "Spenden",
-    // Language Picker (de)
+    // Language Picker
     "language-picker.language": "Sprache",
+    // Hero
+    "hero.welcomeTo": "Willkomen bei ",
+    "hero.aTrulyIndependent": "ein wirklich unabhängiger",
+    "hero.webBrowser": "Web Browser.",
+    "hero.paragraph":
+      "Wir bauen einen brandneuen Browser von Grund auf, der von einer gemeinnützigen Organisation unterstützt wird.",
+    "hero.getInvolved": "Jetzt Mitmachen",
   },
 } as const;

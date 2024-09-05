a727d8f515966509564a40bebf62fca6d6947454
Engin Karakurt <155079545+engin-karakurt@users.noreply.github.com>
2024-09-05 16:59:02 +0200

    i18n: Add ui string file & utils with helper functions

---
+++ src/i18n/ui.ts
@@ -0,0 +1,16 @@
+export const languages = {
+  en: "English",
+  de: "Deutsch",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+  },
+  de: {
+    "nav.about": "Über uns",
+  },
+} as const;

62169394025d4b3115727c2433dced992e7f58c5
Engin Karakurt <155079545+engin-karakurt@users.noreply.github.com>
2024-09-05 23:25:52 +0200

    i18n: Translate about and why section

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -23,6 +23,28 @@ export const ui = {
     "hero.paragraph":
       "We are building a brand-new browser from scratch, backed by a non-profit.",
     "hero.getInvolved": "Get Involved",
+    // About
+    "about.title": "About",
+    "about.firstParagraph":
+      "Ladybird is a brand-new browser & web engine. Driven by a web standards first approach, Ladybird aims to render the modern web with good performance, stability and security.",
+    "about.secondParagraph":
+      "From its humble beginnings as an HTML viewer for the SerenityOS hobby operating system project, Ladybird has since grown into a cross-platform browser supporting Linux, macOS, and other Unix-like systems.",
+    "about.thirdParagraph":
+      "Ladybird is currently in heavy development. We are targeting a first Alpha release for early adopters in 2026.",
+    // Why
+    "why.firstTitlePart": "What makes ",
+    "why.secondTitlePart": "unique",
+    // First card
+    "why.firstCard.title": "Truly independent",
+    "why.firstCard.paragraph":
+      "No code from other browsers. We're building a new engine, based on web standards.",
+    // Second card
+    "why.secondCard.title": "Singular focus",
+    "why.secondCard.paragraph": "We are focused on one thing: the web browser.",
+    // Third card
+    "why.thirdCard.title": "No monetization",
+    "why.thirdCard.paragraph":
+      'No "default search deals", crypto tokens, or other forms of user monetization, ever.',
   },
   de: {
     // Navigation
@@ -41,5 +63,28 @@ export const ui = {
     "hero.paragraph":
       "Wir bauen einen brandneuen Browser von Grund auf, der von einer gemeinnützigen Organisation unterstützt wird.",
     "hero.getInvolved": "Jetzt Mitmachen",
+    // About
+    "about.title": "Über",
+    "about.firstParagraph":
+      "Ladybird ist ein brandneuer Browser und eine neue Web-Engine. Ladybird verfolgt einen Ansatz, bei dem Webstandards im Vordergrund stehen, und zielt darauf ab, das moderne Web mit guter Leistung, Stabilität und Sicherheit darzustellen.",
+    "about.secondParagraph":
+      "Seit seinen bescheidenen Anfängen als HTML-Viewer für das Hobby-Betriebssystemprojekt SerenityOS hat sich Ladybird zu einem plattformübergreifenden Browser entwickelt, der Linux, macOS und andere Unix-ähnliche Systeme unterstützt.",
+    "about.thirdParagraph":
+      "Ladybird befindet sich derzeit in einer intensiven Entwicklungsphase. Wir planen eine erste Alpha-Version für frühe Anwender im Jahr 2026.",
+    // Why
+    "why.firstTitlePart": "Was ",
+    "why.secondTitlePart": "besonders macht",
+    // First card
+    "why.firstCard.title": "Wirklich unabhängig",
+    "why.firstCard.paragraph":
+      "Kein Code aus anderen Browsern. Wir bauen eine neue Engine, die auf Webstandards basiert. ",
+    // Second card
+    "why.secondCard.title": "Einzigartiger Fokus",
+    "why.secondCard.paragraph":
+      "Wir konzentrieren uns auf eine Sache: den Webbrowser.",
+    // Third card
+    "why.thirdCard.title": "Keine Monetarisierung",
+    "why.thirdCard.paragraph":
+      "Keine Suchmaschinen Deals, Krypto-Token oder andere Formen der Nutzermonetarisierung, niemals.",
   },
 } as const;

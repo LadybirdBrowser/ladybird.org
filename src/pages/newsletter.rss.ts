ed6850c17ae4fe80cb4b8697934c24a788167180
nobkd <44443899+nobkd@users.noreply.github.com>
2025-12-14 18:25:20 +0100

    fix feed

--- src/pages/newsletter.rss.ts
+++ src/pages/newsletter.rss.ts
@@ -3,20 +3,18 @@ import { getCollection } from "astro:content";
 import type { APIContext } from "astro";
 
 export async function GET(context: APIContext) {
-  const posts = await getCollection("newsletters");
+  const newsletters = await getCollection("newsletters");
   return rss({
     title: "Ladybird Browser Newsletter",
     description: "Ladybird is a brand-new browser &amp; web engine",
     site: context.site!,
-    items: posts
-      .filter((post) => post.data.type !== "Hidden")
-      .map((post) => ({
-        title: post.data.title,
-        description: post.data.description,
-        author: post.data.author,
-        pubDate: post.data.date,
-        categories: [post.data.type],
-        link: `/newsletter/${post.slug}`,
+    items: newsletters
+      .filter((newsletter) => !newsletter.data.draft)
+      .map((newsletter) => ({
+        title: newsletter.data.title,
+        description: newsletter.data.description,
+        pubDate: newsletter.data.date,
+        link: `/newsletter/${newsletter.slug}`,
       })),
     trailingSlash: false,
   });
